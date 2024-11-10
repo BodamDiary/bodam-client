99fb1835051f9dd7deb9b667dd5ccc9a11d68eed
Dahee Park <ekgml3219@gmail.com>
2024-11-10 19:50:07 +0900

    :sparkles: feat: Add signin page

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,21 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        main_100: "#D0E3FF",
+        main_200: "#9BC3FF",
+        main_300: "#8DBBFF",
+        main_400: "#72ABFF",
+        main_500: "#5499FF",
+        main_600: "#227BFF",
+      },
+      textColor: {
+        main: "#202226",
+        sub: "#636473",
+        placeholder: "#666666",
+      },
+    },
   },
   plugins: [],
 }
-
