984a8c638fe4d161bda373b67e28ff2fc3b6a01c
Dahee Park <ekgml3219@gmail.com>
2024-11-19 13:17:15 +0900

    :arrow_up: add shadui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,73 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors:{
-        main_100: "#D0E3FF",
-        main_200: "#9BC3FF",
-        main_300: "#8DBBFF",
-        main_400: "#72ABFF",
-        main_500: "#5499FF",
-        main_600: "#227BFF",
-      },
-      textColor: {
-        main: "#202226",
-        sub: "#636473",
-        placeholder: "#666666",
-      },
-    },
+  	extend: {
+  		colors: {
+  			main_100: '#D0E3FF',
+  			main_200: '#9BC3FF',
+  			main_300: '#8DBBFF',
+  			main_400: '#72ABFF',
+  			main_500: '#5499FF',
+  			main_600: '#227BFF',
+  			main_700: '#EAF2FF',
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		},
+  		textColor: {
+  			main: '#202226',
+  			sub: '#636473',
+  			placeholder: '#666666'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 }
