f248233f6e7f5b18f5f309d48f6764d2bd886a93
Dahee Park <ekgml3219@gmail.com>
2024-11-19 13:15:57 +0900

    :sparkles: feat: add dashboard page

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,16 @@
+import TodaySportSection from "./_sections/TodaySportSection";
+import BottomNavigationBar from "./_components/BottomNavigationBar";
+import Calendar from "./_components/Calendar";
+
+export default function MainPage() {
+  return (
+    <main className="flex min-h-screen flex-col bg-white gap-5">
+      <div className="flex-1 flex flex-col w-full h-max overflow-y-auto gap-y-6">
+        <TodaySportSection />
+        <div className="border-t mx-6 border-[#d4d6dd]" />
+        <Calendar />
+      </div>
+      <BottomNavigationBar />
+    </main>
+  );
+}
