c510426fec91f16a507c65a8ddf74cd860d838a7
Dahee Park <ekgml3219@gmail.com>
2024-11-25 16:08:43 +0900

    :zap: add apiUrl in signup page

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -3,6 +3,9 @@ import SignUpForm from "./_components/SignUpForm";
 import Link from "next/link";
 import Image from "next/image";
 
+const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+const url = `${apiUrl}/kakao`
+
 export default function SignUpPage(){
     return(
         <main className="flex min-h-screen flex-col items-center select-none overflow-x-hidden overflow-y-hidden">
@@ -17,7 +20,7 @@ export default function SignUpPage(){
                 </div>
                 <p className="flex mt-[35px] w-full justify-center gap-2 text-placeholder font-bold">or</p>
                 <Link
-                    href="/kakao"
+                    href={url}
                     className="mt-[35px] w-full flex justify-center">
                     <div className="relative w-[60px] h-[60px] ">
                         <Image src="/icons/icon-kakao-circle.png" alt="카카오톡 로그인" fill />
